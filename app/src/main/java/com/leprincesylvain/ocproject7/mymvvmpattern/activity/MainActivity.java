aa0733db8e6a9157f1cea0a402d9c93548037d5e
boco0711 <s.lep.0711@gmail.com>
2021-02-01 10:23:13 +0100

    Initial commit

---
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.leprincesylvain.ocproject7.mymvvmpattern.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leprincesylvain.ocproject7.mymvvmpattern.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
