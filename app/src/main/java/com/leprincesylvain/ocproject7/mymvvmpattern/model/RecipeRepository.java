d1864edc0d65ce4df86fc07f4202b9eb288d4594
boco0711 <s.lep.0711@gmail.com>
2021-02-02 23:51:09 +0100

    Add methods

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeRepository.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeRepository.java
@@ -1,4 +1,39 @@
 package com.leprincesylvain.ocproject7.mymvvmpattern.model;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class RecipeRepository {
+    private RecipeDao recipeDao;
+    private LiveData<List<Recipe>> allRecipes;
+
+
+    public RecipeRepository(Application application) {
+        RecipeDatabase database = RecipeDatabase.getInstance(application);
+        recipeDao = database.recipeDao();
+        allRecipes = recipeDao.getAllRecipes();
+    }
+
+    public void insert(Recipe recipe) {
+        
+    }
+
+    public void update(Recipe recipe) {
+
+    }
+
+    public void delete(Recipe recipe) {
+
+    }
+
+    public void deleteAllRecipes() {
+
+    }
+
+    public LiveData<List<Recipe>> getAllRecipes() {
+        return allRecipes;
+    }
 }
