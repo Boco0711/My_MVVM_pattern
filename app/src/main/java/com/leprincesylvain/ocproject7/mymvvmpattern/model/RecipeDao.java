1a17ffb4e73891b7a8ac1c9ff8e01772fd8d12b0
boco0711 <s.lep.0711@gmail.com>
2021-02-02 23:24:23 +0100

    Done insert, update, delete, deleteAll and getAll

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeDao.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeDao.java
@@ -1,4 +1,29 @@
 package com.leprincesylvain.ocproject7.mymvvmpattern.model;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface RecipeDao {
+
+    @Insert
+    void insert(Recipe recipe);
+
+    @Update
+    void update(Recipe recipe);
+
+    @Delete
+    void delete(Recipe recipe);
+
+    @Query("DELETE FROM recipe_table")
+    void deleteAllRecipes();
+
+    @Query("SELECT * FROM recipe_table ORDER BY recipeDifficulty DESC")
+    LiveData<List<Recipe>> getAllRecipes();
 }
