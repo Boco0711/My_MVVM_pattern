fa1530b9e7d45075829c024b33342a8a6f423cd0
boco0711 <s.lep.0711@gmail.com>
2021-02-04 21:56:47 +0100

    Add constructor and methods to RecipeViewModel.java

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/viewmodel/RecipeViewModel.java
@@ -4,10 +4,40 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.leprincesylvain.ocproject7.mymvvmpattern.model.Recipe;
+import com.leprincesylvain.ocproject7.mymvvmpattern.model.RecipeRepository;
+
+import java.util.List;
 
 public class RecipeViewModel extends AndroidViewModel {
+    private RecipeRepository recipeRepository;
+    private LiveData<List<Recipe>> allRecipes;
 
     public RecipeViewModel(@NonNull Application application) {
         super(application);
+        recipeRepository = new RecipeRepository(application);
+        allRecipes = recipeRepository.getAllRecipes();
+    }
+
+    public void insert(Recipe recipe) {
+        recipeRepository.insert(recipe);
+    }
+
+    public void update(Recipe recipe) {
+        recipeRepository.update(recipe);
+    }
+
+    public void delete(Recipe recipe) {
+        recipeRepository.delete(recipe);
+    }
+
+    public void deleteAllRecipes() {
+        recipeRepository.deleteAllRecipes();
+    }
+
+    public LiveData<List<Recipe>> getAllRecipes() {
+        return allRecipes;
     }
 }
