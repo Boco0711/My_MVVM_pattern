8a13c97c19cbf242bf984c03749ffdcd28117e46
boco0711 <s.lep.0711@gmail.com>
2021-02-04 21:50:29 +0100

    Add the possibility to prepopulate the database

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeDatabase.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/model/RecipeDatabase.java
@@ -1,10 +1,13 @@
 package com.leprincesylvain.ocproject7.mymvvmpattern.model;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Recipe.class, version = 1)
 public abstract class RecipeDatabase extends RoomDatabase {
@@ -21,5 +24,26 @@ public abstract class RecipeDatabase extends RoomDatabase {
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private RecipeDao recipeDao;
+
+        private PopulateDbAsyncTask(RecipeDatabase recipeDatabase) {
+            recipeDao = recipeDatabase.recipeDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
