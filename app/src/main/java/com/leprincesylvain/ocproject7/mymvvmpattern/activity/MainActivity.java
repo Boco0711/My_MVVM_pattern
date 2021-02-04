e6b60768bdd9007a6d9120c2b900c9f75ebc930b
boco0711 <s.lep.0711@gmail.com>
2021-02-04 22:01:49 +0100

    Observer set in main activity with on changed method added

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/activity/MainActivity.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/activity/MainActivity.java
@@ -1,16 +1,34 @@
 package com.leprincesylvain.ocproject7.mymvvmpattern.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.leprincesylvain.ocproject7.mymvvmpattern.R;
+import com.leprincesylvain.ocproject7.mymvvmpattern.model.Recipe;
+import com.leprincesylvain.ocproject7.mymvvmpattern.viewmodel.RecipeViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecipeViewModel recipeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipeViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
+        recipeViewModel.getAllRecipes().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(List<Recipe> recipes) {
+                // update RecyclerView
+                Toast.makeText(MainActivity.this, "On Changed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
