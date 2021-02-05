6dfc441a8bf0d774e1f2e973d17536cbc146cd85
boco0711 <s.lep.0711@gmail.com>
2021-02-05 10:16:59 +0100

    RecyclerView on main activity

--- app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/activity/MainActivity.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/mymvvmpattern/activity/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.leprincesylvain.ocproject7.mymvvmpattern.R;
 import com.leprincesylvain.ocproject7.mymvvmpattern.model.Recipe;
+import com.leprincesylvain.ocproject7.mymvvmpattern.view.RecipeAdapter;
 import com.leprincesylvain.ocproject7.mymvvmpattern.viewmodel.RecipeViewModel;
 
 import java.util.List;
@@ -22,12 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        RecipeAdapter adapter = new RecipeAdapter();
+        recyclerView.setAdapter(adapter);
+
         recipeViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
         recipeViewModel.getAllRecipes().observe(this, new Observer<List<Recipe>>() {
             @Override
             public void onChanged(List<Recipe> recipes) {
-                // update RecyclerView
-                Toast.makeText(MainActivity.this, "On Changed", Toast.LENGTH_SHORT).show();
+                adapter.setRecipes(recipes);
             }
         });
     }
